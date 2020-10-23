e11ca501192eb3d79be2cb2ec9db38f0f201bc91
DanielaPF15 <dpfa1507@gmail.com>
2020-10-23 18:09:38 -0500

    metodo completado de generos

--- controllers/book.js
+++ controllers/book.js
@@ -2,8 +2,8 @@ const BookModel = require('../models/book')
 
 
 /**
- * req => Todo lo que se envia
- * res =>Todo lo que se devolverá
+ * @req => Todo lo que se envia
+ * @res =>Todo lo que se devolverá
  */
 exports.create =(req,res)=>{
      if(Object.entries(req.body).length==0){
@@ -32,4 +32,34 @@ exports.create =(req,res)=>{
              })
          }
      )
+}
+
+exports.update =(req,res)=>{
+    if(Object.entries(req.body).length==0){
+        return res.status(400).send({
+            message:'Todos los campos son obligatorios'
+        })
+    }
+
+   const book = {
+    name: req.body.name,
+    author: req.body.author,
+    pageNumber: req.body.pageNumber,
+    publisher: req.body.publisher,
+    publicationDate: req.body.publicationDate,
+    genre: req.body.genre 
+   }
+  BookModel.findByIdAndUpdate(req.params.id,book,{new:true})
+  .then(
+      (bookUpdated)=>{
+          res.send(bookUpdated)
+      }
+  )
+  .catch(
+      (error)=>{
+          return res.status(500).send({
+              message:error.message
+          })
+      }
+  )
 }
