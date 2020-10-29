31e0f2ec46abb2bdbaa29abd2590cd8c5cb45d43
DanielaPF15 <dpfa1507@gmail.com>
2020-10-29 17:39:35 -0500

    make it better

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const mongoose = require ('mongoose')//Paquete que permite la comunicación con nuestra base de datos.
+const config = require('./config')
 const conectDB = ()=>{
     //conect permite conectar a una base de datos tiene opciones:
     // -useNewUrlParser: analizar la informacion que se le quiere enviar a MongoDB.
     //-useUnifiedTopology escucha llamados que hacemos a mongo de BD y monitoréa que es lo que pasa
-    mongoose.connect('',{ useNewUrlParser: true, useUnifiedTopology: 
+    mongoose.connect(config.mongoDB,{ useNewUrlParser: true, useUnifiedTopology: 
         true},(error)=>{
             if(error){
                 console.log('Error',error)
