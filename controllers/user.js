872e6280003d8fc2f2bb40eec16f828ff9273496
DanielaPF15 <dpfa1507@gmail.com>
2020-11-18 17:33:18 -0500

    enviar correos

--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,23 @@
 const UserModel = require('../models/user')
-const service =require('../services/index')
+const service = require('../services/index')
 const nodemailer = require('nodemailer')
 
-
+/**
+ * Método para almacenar un nuevo usuario
+ * @param {*} req => Todo lo que enviamos desde el body (formulario)
+ * @param {*} res => La respuesta que se devolverá
+ */
 exports.create = (req, res) => {
-    /**
-     * Validamos que todos los datos esten completos
-     */
 
+    /**
+     * Validamos que todos los campos del formulario estén llenos.
+     */
     if (Object.entries(req.body).length == 0) {
         return res.status(400).send({
-            message: 'Los datos son obligatorios'
+            message: 'Los datos son obligatoríos.'
         })
     }
+
     const user = new UserModel({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -24,38 +29,42 @@ exports.create = (req, res) => {
     })
 
     user.save()
-        .then((dataUser) => {
-            res.send(dataUser)
-            const contentEmail = '<h1>Hola cómo estás</h1>'
-            sendEmailInfo(dataUser.email, 'Bienvenido', contentEmail, '', res)
-        })
+        .then((dataUser) => { res.send(dataUser) })
         .catch((error) => {
             res.status(500).send({
                 message: error.message
             })
         })
 }
+
+/**
+ * Método para actualizar un usuario.
+ * @param {*} req => Todo lo que enviamos desde el body (formulario)
+ * @param {*} res => La respuesta que se devolverá
+ */
 exports.update = (req, res) => {
+    /**
+     * Validamos que todos los campos del formulario estén llenos.
+     */
     if (Object.entries(req.body).length == 0) {
         return res.status(400).send({
-            message: 'Los datos son obligatorios'
+            message: 'Los datos son obligatoríos.'
         })
     }
+
     const user = {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
         password: req.body.password,
-        role: req.body.role,
         birthDate: req.body.birthDate,
         age: req.body.age
-
     }
+
     /**
-     * findByIdAndUpdate =>Metodo de mongoose que permite 
-     * buscar por id de usuario tiene los parámetros:
-     *  -El id del usuario =>req.params.id es el id que se envía por url
-     * -los datos nuevos.
+     * findByIdAndUpdate => Método de mongoose que permite buscar por id y actualizar u usuario. Tiene los párametros:
+     *  - El id del usuario. => req.params.id es el id que se envía por la URL.
+     *  - Los datos nuevos.
      */
     UserModel.findByIdAndUpdate(req.params.id, user)
         .then(
@@ -63,67 +72,77 @@ exports.update = (req, res) => {
                 res.send(userUpdate)
             }
         ).catch(
-            (error)=>{
-                   res.status(500).send({
-                       message: error.message
-                   })
+            (error) => {
+                res.status(500).send({
+                    message: error.message
+                })
+            }
+        )
+
+}
+
+/**
+ * Método para listar todos los usuarios
+ */
+exports.getAll = (req, res) => {
+    UserModel.find()
+        .then((users) => { res.send(users) })
+        .catch(
+            (error) => {
+                res.status(500).send({
+                    message: error.message
+                })
             }
         )
 }
-exports.getAll = (req,res)=>{
-   UserModel.find()
-    .then((users)=>res.send(users))
-    .catch(
-        (error)=>{
-            return res.status(500).send({
-                message:error.message
-            })
-        }
-    )
-}
-exports.getOne = (req,res)=>{
+
+exports.getOne = (req, res) => {
     UserModel.findById(req.params.id)
-    .then((user)=>res.send(user))
-    .catch(
-        (error)=>{
-            return res.status(500).send({
-                message:error.message
+
+        .then((users) => { res.send(users) })
+        .catch(
+            (error) => {
+                res.status(500).send({
+                    message: error.message
+                })
+            }
+        )
+}
+
+exports.deleteOne = (req, res) => {
+    UserModel.findByIdAndRemove(req.params.id)
+        .then((users) => { res.send(users) })
+        .catch(
+            (error) => {
+                res.status(500).send({
+                    message: error.message
+                })
+            }
+        )
+}
+
+exports.login = (req, res) => {
+    UserModel.findOne({ email: req.body.email }, (error, dataUser) => {
+        if (dataUser != null) {
+            if (dataUser.password == req.body.password) {
+                res.send({ token: service.createToken(dataUser) })
+            } else {
+                res.status(400).send({
+                    message: 'Los datos no coinciden'
+                })
+            }
+        } else {
+            res.status(400).send({
+                message: 'Los datos no coinciden'
             })
         }
-    )
-}
-
-exports.deleteOne =(req,res) =>{
-    UserModel.findByIdAndRemove(req.params.id)
-    .then((user) => {res.send(user)})
-    .catch((error) => {
-        res.status(500).send({message: error.message})
     })
 }
 
-exports.login=(req,res)=>{
-    UserModel.findOne({email:req.body.email},(error,dataUser)=>{
- if (dataUser !=null) {
-     if (dataUser.password==req.body.password) {
-         res.send({token:service.createToken(dataUser)})
-         
-     }else{
-         res.status(400).send({
-             message:'Los datos no coinciden'
-         })
-     }
- } else {
-     res.status(400).send({
-         message:'Los datos no coinciden'
-     })
- }
-    })
-}
-
-exports.sendEmail =(req,res) =>{
+exports.SendEmail =(req,res) =>{
     const email = req.query.email 
     const name = req.query.name
-    requirements(email,name,res)
+    requirements(email,name,res);
 
 }
 
@@ -132,7 +151,7 @@ const requirements = (email,name,res) => {
     const contentEmail = `<h1>Mensaje desde el formulario de contacto</h1>
         Hola, hemos recibido un mensaje de ${name} con el correo ${email}, por favor comunicate.`
 
-        sendEmailInfo('dpfa1507@gmail.com','Formulario contacto', contentEmail,'',res)
+        sendEmailInfo('green.life.artemisas@gmail.com','Formulario contacto', contentEmail,'',res)
     }
 
 const sendEmailInfo = (receiver, subject, contentEmail, contentTxt = '', res) => {
@@ -166,11 +185,9 @@ const sendEmailInfo = (receiver, subject, contentEmail, contentTxt = '', res) =>
         }else{
             res.status(200).send({
                 message: 'Correo enviado correctamente'
-            })  
-            
+            }) 
         }
     })
 
- 
 
 }
